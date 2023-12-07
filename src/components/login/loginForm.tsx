f697798c084e4ea0be0d6148cc801c58ec260d91
pelusa-v <razor0164@gmail.com>
2023-12-07 00:11:08 -0500

    Signup component done

--- src/components/login/loginForm.tsx
+++ src/components/login/loginForm.tsx
@@ -5,9 +5,21 @@ interface IAppProps {
 
 const LoginForm : React.FunctionComponent<IAppProps> = (props) => {
   return (
-    <p className="text-red-700">
-        hELLO!
-    </p>
+    <div className="bg-zinc-600 p-8 rounded-md">
+        <div className="mb-5 text-white">Register!</div>
+        <div className="mb-2">
+            <div>
+                <label className="justify-start flex text-xs text-white"  htmlFor="email">Email</label>
+            </div>
+            <input id="email" className="rounded-sm p-1" type="text" placeholder="esample@org.com" />
+        </div>
+        <div className="mb-2">
+            <div>
+                <label className="justify-start flex text-xs text-white" htmlFor="password">Password</label>
+            </div>
+            <input id="password" className="rounded-sm p-1" type="text" placeholder="8 characters"/>
+        </div>
+    </div>
   );
 };
 
